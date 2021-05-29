06b7156f8826fc971d2d4a3618b370beae335ca6
mstanka <mice78.mm@gmail.com>
2021-05-29 09:38:09 +0200

    initial setup

---
+++ src/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import { render } from 'react-dom';
+import './style.css';
+
+const App = () => <div className="container">dadjokes-react</div>;
+
+render(<App />, document.querySelector('#app'));
