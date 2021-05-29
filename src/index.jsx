dbb35859b5c097944271fadea06e492ab4e5d43f
mstanka <mice78.mm@gmail.com>
2021-05-29 10:01:47 +0200

    add functionality on like and dislike btn

--- src/index.jsx
+++ src/index.jsx
@@ -1,43 +1,54 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { render } from 'react-dom';
 import './style.css';
 
 const App = () => {
-  
+  const [upLikes, setUpLikes] = useState(0);
+  const [downLikes, setDownLikes] = useState(0);
 
   return (
-    <div class="container">
-      <div class="joke">
-        <div class="joke__body">
-          <div class="joke__user">
+    <div className="container">
+      <div className="joke">
+        <div className="joke__body">
+          <div className="joke__user">
             <img
-              class="user-avatar"
+              className="user-avatar"
               src="https://raw.githubusercontent.com/Czechitas-podklady-WEB/dadjokes/main/users/user01.png"
             />
-            <p class="user-name">Neroxx</p>
+            <p className="user-name">Neroxx</p>
           </div>
 
-          <p class="joke__text">
+          <p className="joke__text">
             The secret service isn't allowed to yell "Get down!" anymore when
             the president is about to be attacked. Now they have to yell
             "Donald, duck!"
           </p>
         </div>
-        <div class="joke__likes">
-          <button id="btn-up" class="btn-like btn-like--up"></button>
-          <span id="likes-up" class="likes-count likes-count--up">
-            0
+        <div className="joke__likes">
+          <button
+            id="btn-up"
+            className="btn-like btn-like--up"
+            onClick={() => {
+              setUpLikes(upLikes + 1);
+            }}
+          ></button>
+          <span id="likes-up" className="likes-count likes-count--up">
+            {upLikes}
           </span>
-          <button id="btn-down" class="btn-like btn-like--down"></button>
-          <span id="likes-down" class="likes-count likes-count--down">
-            0
+          <button
+            id="btn-down"
+            className="btn-like btn-like--down"
+            onClick={() => {
+              setDownLikes(downLikes + 1);
+            }}
+          ></button>
+          <span id="likes-down" className="likes-count likes-count--down">
+            {downLikes}
           </span>
         </div>
       </div>
     </div>
   );
-}
-
-
+};
 
 render(<App />, document.querySelector('#app'));
